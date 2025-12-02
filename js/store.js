bee73c60aae15cccff33fad78cdc92e907014de6
Julio Merino <julimega@myuax.com>
2025-12-02 10:16:23 +0100

    Desarrollo store

--- js/store.js
+++ js/store.js
@@ -2,20 +2,26 @@ async function cargarSemillas() {
     try {
         const response = await fetch('../data/semillas.json');
         const semillas = await response.json();
-        const productList = document.getElementById('product-list');
+        const herbList = document.getElementById('herb-list');
+        const vegList = document.getElementById('veg-list');
         
         semillas.forEach(semilla => {
             const productDiv = document.createElement('div');
             productDiv.className = 'product-card';
             
             productDiv.innerHTML = `
-                <img src="../img/herb/${semilla.nombre.toLowerCase()}.png" alt="${semilla.nombre}" class="product-image">
+                <img src="../img/${semilla.tipo_planta}/${semilla.nombre.toLowerCase()}.png" alt="${semilla.nombre}" class="product-image">
                 <h2 class="product-name">${semilla.nombre}</h2>
-                <p class="product-price">${semilla.precio}</p>
+                <p class="product-price">${semilla.precio}€</p>
                 <button class="add-to-cart-btn">Añadir al carrito</button>
             `;
             
-            productList.appendChild(productDiv);
+            // Añadir al div correspondiente según el tipo de planta
+            if (semilla.tipo_planta === 'herb') {
+                herbList.appendChild(productDiv);
+            } else if (semilla.tipo_planta === 'veg') {
+                vegList.appendChild(productDiv);
+            }
         });
     } catch (error) {
         console.error('Error al cargar las semillas:', error);
