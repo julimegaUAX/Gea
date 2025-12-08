409fdcbc0e8660dfb43ca1259177f50d753da6d3
Julio Merino <julimega@myuax.com>
2025-12-08 17:47:51 +0100

    Limpieza Index

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         }
     });
 
-    // Menú desplegable
+    // Menú desplegable activado
     document.getElementById('menu-icon').addEventListener('click', function() {
         const menu = document.querySelector('.menu-desplegable');
         const overlay = document.getElementById('menu-overlay');
@@ -24,7 +24,8 @@ $(document).ready(function() {
         document.body.style.overflow = 'hidden';
     });
 
-    document.getElementById('close-icon').addEventListener('click', function() {
+    // Función para cerrar el menú
+    function cerrarMenu() {
         const menu = document.querySelector('.menu-desplegable');
         const overlay = document.getElementById('menu-overlay');
         const menuIcon = document.getElementById('menu-icon');
@@ -35,19 +36,7 @@ $(document).ready(function() {
         menuIcon.style.display = 'block';
         closeIcon.style.display = 'none';
         document.body.style.overflow = 'auto';
-    });
-
-    // Cerrar menú al hacer click en el overlay
-    document.getElementById('menu-overlay').addEventListener('click', function() {
-        const menu = document.querySelector('.menu-desplegable');
-        const overlay = document.getElementById('menu-overlay');
-        const menuIcon = document.getElementById('menu-icon');
-        const closeIcon = document.getElementById('close-icon');
-        
-        menu.classList.remove('active');
-        overlay.classList.remove('active');
-        menuIcon.style.display = 'block';
-        closeIcon.style.display = 'none';
-        document.body.style.overflow = 'auto';
-    });
+    }
+    document.getElementById('close-icon').addEventListener('click', cerrarMenu);
+    document.getElementById('menu-overlay').addEventListener('click', cerrarMenu);
 });
